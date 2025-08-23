7bd2091bdb17429a6cb4058daca6206338a5b01d
vikramthakur789 <vikramthakur95080@gmail.com>
2025-08-23 19:30:58 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,43 @@
+# Creating variables in  python
+# name, value, memory location, data type
+x = 1
+print(type(x))
+name = "Vikram"
+print(type(name))
+price = 999.99
+print(type(price))
+is_adult = False
+print(type(is_adult))
+
+
+a = 57
+b = 776
+c = 62356
+g = 326565
+
+
+print(a+b-c+g, sep = "gk, ")
+
+g, h, j, k, o, = 43,44,55,33,"golu babu"
+print(g,h,j,k,o, sep = "77,")
+name="ankit"
+Name="Golu BABU"
+print(Name)
+
+name = "golu babu"
+age = 21
+hobie = "basketball"
+gole =" software engineer"
+print(name, age, hobie, gole,)
+
+
+
+
+
+
+
+
+
+
+
+
