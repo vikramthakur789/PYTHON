b101f65f36988ed0786ecdcfffd33992d1017832
vikramthakur789 <vikramthakur95080@gmail.com>
2025-08-23 22:33:41 +0530

    Create complex.py

---
+++ complex.py
@@ -0,0 +1,19 @@
+z = 1 + 2j
+print(z.real)
+print(z.imag)
+
+z1 = 2+3j
+z2 = 1-2j
+sum_z = z1+z2
+print(sum_z)
+diff_z = z1 - z2
+print(diff_z)
+prod_z = z1*z2
+print(prod_z)
+quot_z = z1 / z2
+print(quot_z)
+mag_z1 = abs(z1)
+print(mag_z1)
+
+z3 = complex(3,2)
+print(z3)
