195a2d8c6970dff88ea0bda1ff0ea40ee1a5762a
vikramthakur789 <vikramthakur95080@gmail.com>
2025-08-25 21:40:26 +0530

    Create collection.py

---
+++ collection.py
@@ -0,0 +1,30 @@
+day1_temp = 71
+day2_temp = 72
+day3_temp = 73
+day4_temp = 74
+temps = [71, 72, 73, 74]
+print(sum(temps) / len(temps))
+
+# sequence - string, list, tuple
+# mapping - dictionary
+# sets    - set, frozenset
+
+# string - ordered, immutable sequences of characters
+#        - " Hello world"
+
+# list - ordered, mutable collections that can contain mixed data type
+#      - [1, 'hello, 3.45, True]
+
+# tuple - ordered, immutable collections similar to lists
+#       - (1, 'hello', 3.45, True)
+
+# dictionary - unordered (pre-3.7) / insertion-ordered (3.7+) key-value pairs
+#             - {'name' :'Alice', 'age': 25, 'city ': ' New York'}
+
+d = {'name' :'Alice', 'age': 25, 'city ': ' New York'}
+print(d['age '])
+
+# set - unordered mutable collection of unique element
+#     - {1, 2, 3, 4}
+
+# forzeneset - Immutable version of a set
