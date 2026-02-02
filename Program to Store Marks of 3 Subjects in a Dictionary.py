729fb00ee4707c4bbd12d615ce266978b5377f2a
vikramthakur789 <vikramthakur95080@gmail.com>
2026-02-02 23:53:01 +0530

    Create Program to Store Marks of 3 Subjects in a Dictionary.py

---
+++ Program to Store Marks of 3 Subjects in a Dictionary.py
@@ -0,0 +1,13 @@
+
+marks = {}
+
+x = int(int(input("enter phy :")))
+marks.update({"phy": x})
+
+x = int(int(input("enter che :")))
+marks.update({"che": x})
+
+x = int(int(input("enter math :")))
+marks.update({"math": x})
+
+print(marks)
