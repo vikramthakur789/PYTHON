1cfb310ece827e54aeb2b94e9e72858d74706ec5
vikramthakur789 <vikramthakur95080@gmail.com>
2026-02-05 23:19:18 +0530

    Create WAP to print all elements of a list using a loop.py

---
+++ WAP to print all elements of a list using a loop.py
@@ -0,0 +1,12 @@
+nums = [1,4,9,16,25,36,49,64,81,100]
+heroes = ["ironman", "thor", "superman", "batman"]
+traverse = ek-ek item par jaana
+idx = 0
+while idx < len(heroes):
+    print(heroes[idx])
+    idx += 1
+
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
