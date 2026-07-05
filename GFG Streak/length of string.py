a999f3a2e2ca8aa70bf219716f180599610f4f74
vikramthakur789 <vikramthakur95080@gmail.com>
2026-07-05 12:27:27 +0530

    Add lengthString method to calculate string length
    Implement a method to calculate the length of a string.

---
+++ GFG Streak/length of string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthString(self, s):
+        # code here
+        
+        len = 0
+        
+        for ch in s:
+            len += 1
+            
+        return len   
+    
