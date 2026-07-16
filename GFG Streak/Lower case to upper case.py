f1ac8c7c6e6370c0bb157d272a9267d583aab2da
vikramthakur789 <vikramthakur95080@gmail.com>
2026-07-16 10:00:28 +0530

    Create Lower case to upper case.py

---
+++ GFG Streak/Lower case to upper case.py
@@ -0,0 +1,10 @@
+class Solution:
+    def to_upper(self, str):
+        # code here
+        
+        result = ""
+        
+        for ch in str:
+            result += ch.upper()
+            
+        return result    
