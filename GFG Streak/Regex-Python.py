79bce36b58fe227ae3bbd0763fcac0497e813c4c
vikramthakur789 <vikramthakur95080@gmail.com>
2026-08-26 12:48:39 +0530

    Add numberMatcher function to extract numbers
    Implement a function to find and print all numbers in a string.

---
+++ GFG Streak/Regex-Python.py
@@ -0,0 +1,9 @@
+import re
+def numberMatcher(str):
+    pat=r"\d+"#write the pattern here
+    match=re.findall(pat,str) ##find all finds all the matched texts and returns a list
+    if(match): 
+        for i in match:
+            print(i, end=" ")
+    else:
+        print(-1,end="")
