e4aff26b39f62643b56355312d18763c3f1e9817
vikramthakur789 <vikramthakur95080@gmail.com>
2026-09-21 11:35:08 +0530

    Create Jumping through While.py

---
+++ GFG Streak/Jumping through While.py
@@ -0,0 +1,11 @@
+def printIncreasingPower(x):
+    #code here
+    # Loop to jump in powers of 2
+    i = 1
+    while(i * i <= x):
+        #code here
+        print ( i * i, end = " ")
+        
+        #code here
+        i += 1
+      
